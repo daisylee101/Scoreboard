a5454b989179462874edf10447592e16d8cf6bda
daisylee101 <daisyl43@nycstudents.net>
2024-10-16 16:12:34 +0000

    new

--- Main.java
+++ Main.java
@@ -1,1 +1,12 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String info;
+        Scoreboard game = new Scoreboard("Red", "Blue");
+        info = game.getScore();
+        System.out.println(info);
+        
+    }
+}
 
---
+++ Scoreboard.java
@@ -0,0 +1,16 @@
+public class Scoreboard
+{
+    private String teamOne;
+    private String teamTwo;
+    public Scoreboard(String one, String two)
+    {
+        teamOne = one;
+        teamTwo = two;
+    }
+    public String getScore()
+    {
+        return null;
+    }
+
+
+}
